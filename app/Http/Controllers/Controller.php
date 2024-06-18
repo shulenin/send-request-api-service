0d2ff557aa9dfd8474d8e8f315af0215262b303a
shulenin <dima.shulenin@mail.ru>
2024-06-18 20:15:12 +0300

    build: init php-cs-fixer

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -22,5 +22,6 @@ use Illuminate\Routing\Controller as BaseController;
  */
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests;
+    use ValidatesRequests;
 }
