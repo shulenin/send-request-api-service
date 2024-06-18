4ee4e16c69ef71a8b51901f35af453880eca6379
shulenin <dima.shulenin@mail.ru>
2024-06-18 18:57:10 +0300

    build: Инициализация Swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,20 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Swagger(
+ *      schemes={"http"},
+ *      basePath="/api/v1",
+ *      @OA\Info(
+ *          title="Send Request API Service",
+ *          version="1.0.0",
+ *          @OA\Contact(
+ *              email="dima.shulenin@mail.ru"
+ *          ),
+ *      ),
+ *      @OA\PathItem(path="/api")
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
